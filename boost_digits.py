72ddeb2f11c5f3442314ca4f13dbcbb3a2349b21
thanhtran18 <tranct@myumanitoba.ca>
2019-03-22 10:38:43 -0500

    complete

--- boost_digits.py
+++ boost_digits.py
@@ -1,80 +1,83 @@
-from AdaBoost.utils import *
+import numpy as np
+from scipy.io import *
+import matplotlib.pyplot as plt
+from utils import *
 
 # boosting for recognizing MNIST digits
 
 # Load the data X and labels t
-data=loadmat('digits.mat')
-X,t=data['X'],data['t']
-t=t.astype(int)
+data = loadmat('digits.mat')
+X, t = data['X'], data['t']
+t = t.astype(int)
 
 # X is 28x28x1000, t is 1000x1
 # Each X[:,:,i] os a 28x28 image
 
 # Subsample images to be 14x14 for speed
-X=X[::2,::2,:]
+X = X[::2, ::2, :]
 
 # Set up target values
 # 4s are class +1, all others are class -1
-f4=(t==4)
-n4=(t!=4)
-t[f4]=1
-t[n4]=-1
+f4 = (t == 4)
+n4 = (t != 4)
+t[f4] = 1
+t[n4] = -1
 
 # 14,14,1000
-nx,ny,ndata = X.shape
+nx, ny, ndata = X.shape
 
 # Number to use as training images
 ntrain = 500
 
 # Training and test images
-X_train = X[:,:,:ntrain]
+X_train = X[:, :, :ntrain]
 t_train = t[:ntrain]
-X_test = X[:,:,ntrain:]
+X_test = X[:, :, ntrain:]
 t_test = t[ntrain:]
 
 # Boosting code goes here
 niter = 100
 
 # Initialize the weights
-weights = np.ones((1,ntrain))/ntrain
-classifier = {'alpha':np.zeros(niter), 'd':np.zeros((niter,2)).astype(int), 'p':np.zeros(niter), 'theta':np.zeros(niter)}
+weights = np.ones((1, ntrain)) / ntrain
+classifier = {'alpha': np.zeros(niter), 'd': np.zeros((niter, 2)).astype(int), 'p': np.zeros(niter),
+              'theta': np.zeros(niter)}
 
 for iter in range(niter):
     # Find the best weak learner
-    d,p,theta,correct = findWeakLearner(X_train,t_train,weights)
-
+    d, p, theta, correct = findWeakLearner(X_train, t_train, weights)
 
     ###########################################################
     ##################### Fill in #############################
     ###########################################################
-    err = (np.dot(weights, (1 - correct)) / sum(weights))
+    err = weights @ (1 - correct) / weights.sum()
     if err > 0.5:
         break
     # alpha = 0
-    alpha = (np.log((1 - err) / (err + 0.001))).reshape(1, 1)
-    # test= (1-correct).T
-    weights = np.multiply(weights, np.exp(np.dot(alpha, ((1-correct).reshape(500, 1)).T)))
-    weights = weights / sum(weights)
+    alpha = np.log((1 - err) / (err + np.spacing(1)))
+
+    weights = weights * np.exp(alpha.reshape(1, 1) @ (1 - correct).reshape(1, 500))
+    weights = weights/weights.sum()
     ###########################################################
     ##################### End fill in #########################
     ###########################################################
-    
-    classifier['alpha'][iter]=alpha
-    classifier['d'][iter,:]=d
-    classifier['p'][iter]=p
-    classifier['theta'][iter]=theta
+
+    classifier['alpha'][iter] = alpha
+    classifier['d'][iter, :] = d
+    classifier['p'][iter] = p
+    classifier['theta'][iter] = theta
 
 # Show plots of training error and test error
 
-train_errs = evaluateClassifier(classifier,X_train,t_train)
-test_errs = evaluateClassifier(classifier,X_test,t_test)
+train_errs = evaluateClassifier(classifier, X_train, t_train)
+test_errs = evaluateClassifier(classifier, X_test, t_test)
 
 plt.figure(1)
-plt.rcParams['font.size']=20
-plt.plot(train_errs,'r-')
-plt.plot(test_errs,'b-')
+plt.rcParams['font.size'] = 20
+plt.plot(train_errs, 'r-')
+plt.plot(test_errs, 'b-')
 plt.xlabel('Number of iterations')
 plt.ylabel('Error')
-plt.legend(['Training error','Test error'])
+plt.legend(['Training error', 'Test error'])
 
-visualizeClassifier(classifier,2,(nx,ny))
+visualizeClassifier(classifier, 2, (nx, ny))
--- utils.py
+++ utils.py
@@ -2,7 +2,8 @@ import numpy as np
 from scipy.io import *
 import matplotlib.pyplot as plt
 
-def findWeakLearner(X,t,weights):
+
+def findWeakLearner(X, t, weights):
     # Fine the best weaker learner given data, targets, and weights.
     # weakl learner is a decision stump, p x(d) > p theta
     #
@@ -14,13 +15,13 @@ def findWeakLearner(X,t,weights):
     # theta is scale
     # correct is N-by-1, binary. correct[i]=1 iff this weak learner correctly classifies example X[:,:,i]
 
-    N=X.shape[2]
-    
+    N = X.shape[2]
+
     t = np.squeeze(t)
     weights = np.squeeze(weights)
-    
+
     # Sort all coordinates of X
-    sinds = np.argsort(X) # sort based on last column
+    sinds = np.argsort(X)  # sort based on last column
     Xs = np.sort(X)
 
     # Sort target vales according to this data sort
@@ -32,43 +33,44 @@ def findWeakLearner(X,t,weights):
     # Compute cumsum to evalute goodness of possible thresholds theta.
     # cweight_pos[i,j,k] is amount of correct - incorrect weight incurred on left
     # side of threshold at (Xs[i,j,k]+Xs[i,j,k+1])/2
-    cweight_pos = np.cumsum(Ts*Ws,2)
+    cweight_pos = np.cumsum(Ts * Ws, 2)
 
     # Do same in reverse (total -)
     # cwiehgt_neg[i,j,k] is amount of correct - incorrect weight incurred on right
     # side of threshold at (Xs[i,j,k]+Xs[i,j,k+1])/2
-    cweight_neg = np.expand_dims(cweight_pos[:,:,-1],2) - cweight_pos
+    cweight_neg = np.expand_dims(cweight_pos[:, :, -1], 2) - cweight_pos
 
     # Max of either +1/1 times sum of two.
     signed_cor = cweight_pos - cweight_neg
     # Locations where Xs[i,j,k]==Xs[i,j,k+1] are not valid as possible threshold locations
     # Set these to zero so that we do not find thme as maxima
-    valid = (np.diff(Xs,1,2) != 0)
-    signed_cor = signed_cor * np.concatenate((valid,np.zeros((X.shape[0],X.shape[1],1))),2)
+    valid = (np.diff(Xs, 1, 2) != 0)
+    signed_cor = signed_cor * np.concatenate((valid, np.zeros((X.shape[0], X.shape[1], 1))), 2)
 
     us_cor = np.abs(signed_cor)
 
-    i,j,k=np.unravel_index(np.argmax(us_cor), signed_cor.shape)
+    i, j, k = np.unravel_index(np.argmax(us_cor), signed_cor.shape)
 
     # Compute theta, check boundary cayse
-    if k==N-1:
+    if k == N - 1:
         theta = np.inf
     else:
-        theta = (Xs[i,j,k]+Xs[i,j,k+1])/2
+        theta = (Xs[i, j, k] + Xs[i, j, k + 1]) / 2
 
     # The feature is i,j
-    d=[i,j]
-    
+    d = [i, j]
+
     # Check whether it was a +max or |-max| to get partity p
-    p = -np.sign(signed_cor[i,j,k])
+    p = -np.sign(signed_cor[i, j, k])
 
     # Whether or not this weak learner classifies examples correctly
-    tmp=p*X[i,j,:]>p*theta
-    correct = (t*(tmp-0.5))>0
+    tmp = p * X[i, j, :] > p * theta
+    correct = (t * (tmp - 0.5)) > 0
 
-    return d,p,theta,correct
+    return d, p, theta, correct
 
-def evaluateClassifier(classifier,x,t):
+
+def evaluateClassifier(classifier, x, t):
     # Evaluate classifer on data
     #
     # classifier is a map of alpha,d,p,theta arrays of length M
@@ -82,50 +84,53 @@ def evaluateClassifier(classifier,x,t):
     N = x.shape[2]
 
     # Responses f inputs to each classifier in committee
-    resps = np.zeros((N,M))
+    resps = np.zeros((N, M))
     for m in range(M):
-        x_use = x[classifier['d'][m,0],classifier['d'][m,1],:]
-        resps[:,m]=classifier['alpha'][m]*np.sign((classifier['p'][m]*x_use > classifier['p'][m]*classifier['theta'][m])-0.5)
+        x_use = x[classifier['d'][m, 0], classifier['d'][m, 1], :]
+        resps[:, m] = classifier['alpha'][m] * np.sign(
+            (classifier['p'][m] * x_use > classifier['p'][m] * classifier['theta'][m]) - 0.5)
 
     # Compute output of classifier using first i components using cumsum
-    class_out = np.sign(np.cumsum(resps,1))
+    class_out = np.sign(np.cumsum(resps, 1))
 
     # Compare classifier output to ground-truth t
-    correct = (class_out*np.reshape(t,(N,1)))>0
+    correct = (class_out * np.reshape(t, (N, 1))) > 0
 
-    errs = 1-np.mean(correct,0)
+    errs = 1 - np.mean(correct, 0)
 
     return errs
 
-def visualizeClassifier(classifier,fig_num,im_size):
+
+def visualizeClassifier(classifier, fig_num, im_size):
     # Visualize a classifier
     # Color negative and positive pixels by sum of alpha values
 
     pos_feat = np.zeros(im_size)
     neg_feat = np.zeros(im_size)
     for m in range(len(classifier['alpha'])):
-        if classifier['p'][m]>0:
-            pos_feat[classifier['d'][m,0],classifier['d'][m,1]] += classifier['alpha'][m]
+        if classifier['p'][m] > 0:
+            pos_feat[classifier['d'][m, 0], classifier['d'][m, 1]] += classifier['alpha'][m]
         else:
-            neg_feat[classifier['d'][m,0],classifier['d'][m,1]] += classifier['alpha'][m]
+            neg_feat[classifier['d'][m, 0], classifier['d'][m, 1]] += classifier['alpha'][m]
 
     plt.figure(fig_num)
     plt.subplot(121)
-    plt.imshow(pos_feat,cmap='gray')
+    plt.imshow(pos_feat, cmap='gray')
     plt.title('Sum of weights on p=1 features')
 
     plt.subplot(122)
-    plt.imshow(neg_feat,cmap='gray')
+    plt.imshow(neg_feat, cmap='gray')
     plt.title('Sum of weights on p=-1 features')
     plt.show()
-    
-if __name__ == "__main__":
-    N=5
-    X=np.random.random((10,20,N))
-    t=np.random.random((N,1))
-    weights=np.random.random((N,1))
 
-    d,p,theta,correct=findWeakLearner(X,t,weights)
+
+if __name__ == "__main__":
+    N = 5
+    X = np.random.random((10, 20, N))
+    t = np.random.random((N, 1))
+    weights = np.random.random((N, 1))
+
+    d, p, theta, correct = findWeakLearner(X, t, weights)
     print(d)
     print(p)
     print(theta)
